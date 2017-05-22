238393427d1c995ac794475e08e8e45c5b64df6a
ntmhanh <ntmhanh@tma.com.vn>
2017-05-22 14:37:36 +0700

    Drawing your circle and display car parks around your site

--- app/src/main/java/com/tma/sparking/fragments/MapsFragment.java
+++ app/src/main/java/com/tma/sparking/fragments/MapsFragment.java
@@ -1,24 +1,15 @@
 package com.tma.sparking.fragments;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
+import android.graphics.Color;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -26,10 +17,16 @@ import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.SphericalUtil;
 import com.tma.sparking.R;
 
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static android.content.Context.LOCATION_SERVICE;
 
@@ -37,6 +34,8 @@ import static android.content.Context.LOCATION_SERVICE;
 public class MapsFragment extends Fragment {
     MapView mMapView;
     private GoogleMap googleMap;
+    private List<Marker> markers = new ArrayList<>();
+    private List<LatLng> carCoordinates = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -69,23 +68,59 @@ public class MapsFragment extends Fragment {
         LocationManager locationManager = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
         Criteria criteria = new Criteria();
         Location lastLocation = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+        LatLng yourCoordinate = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+
+        //Log.d("MyLatLng", lastLocation.getLatitude() + ", " +  lastLocation.getLongitude());
+        LatLng carCoordinate = new LatLng(lastLocation.getLatitude() - 0.06358746588293, lastLocation.getLongitude() - 0.0000085939481);
+        carCoordinates.add(carCoordinate);
         if (lastLocation != null)
                 {
                     googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                            new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude()), 13));
-
+                            yourCoordinate, 13));
                     CameraPosition cameraPosition = new CameraPosition.Builder()
                             .target(new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude()))      // Sets the center of the map to location user
                             .zoom(15)                   // Sets the zoom
                             .bearing(90)                // Sets the orientation of the camera to east
                             .tilt(40)                   // Sets the tilt of the camera to 30 degrees
                             .build();                   // Creates a CameraPosition from the builder
-                    googleMap.addMarker(new MarkerOptions().position(new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude()))
+                    googleMap.addMarker(new MarkerOptions().position(yourCoordinate)
                             .icon(BitmapDescriptorFactory
                                     .defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
                             .title("Marker Title").snippet("Marker Description"));
                     googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+                    displayCarParksAroundYourSite(yourCoordinate, carCoordinates);
                 }
+    }
+
+    /**
+     * Drawing your circle and display car parks around your site
+     * @param latLng
+     * @param positions
+     */
+    private void displayCarParksAroundYourSite(LatLng latLng, List<LatLng> positions) {
+        for (LatLng position : positions) {
+            Marker marker = googleMap.addMarker(
+                    new MarkerOptions()
+                            .position(position)
+                            .icon(BitmapDescriptorFactory
+                                    .defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
+                            .visible(false)); // Invisible for now
+            markers.add(marker);
+        }
+
+        //Draw your circle
+         googleMap.addCircle(new CircleOptions()
+                .center(latLng)
+                .radius(3000)
+                .strokeColor(Color.rgb(0, 136, 255))
+                .fillColor(Color.argb(20, 0, 136, 255)));
+
+        for (Marker marker : markers) {
+            if (SphericalUtil.computeDistanceBetween(latLng, marker.getPosition()) < 3000) {
+                marker.setVisible(true);
+            }
+        }
     }
 
     @Override
