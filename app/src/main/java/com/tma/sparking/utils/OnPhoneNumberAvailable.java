67ce2102778224da1800a4629d3f32c378ff3ee1
Kim Huy <pkimhuy@tma.com.vn>
2017-05-26 09:41:33 +0700

    read phone number on device with api level 23 and above

---
+++ app/src/main/java/com/tma/sparking/utils/OnPhoneNumberAvailable.java
@@ -0,0 +1,15 @@
+package com.tma.sparking.utils;
+
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+
+/**
+ * Created by pkimhuy on 5/26/2017.
+ */
+
+public abstract class OnPhoneNumberAvailable implements ActivityCompat.OnRequestPermissionsResultCallback {
+
+
+
+}
--- app/src/main/java/com/tma/sparking/utils/PhoneInformation.java
+++ app/src/main/java/com/tma/sparking/utils/PhoneInformation.java
@@ -1,11 +1,61 @@
 package com.tma.sparking.utils;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.telephony.TelephonyManager;
 
-public class PhoneInformation {
-    public static String getPhoneNumber(Context context) {
-        TelephonyManager telephonyManager = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+public abstract class PhoneInformation implements ActivityCompat.OnRequestPermissionsResultCallback {
+    public static final int REQUEST_READ_PHONE_STATE = 1;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PhoneInformation.REQUEST_READ_PHONE_STATE) {
+            if (permissions.length == 1 & grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                String phoneNumber = getPhoneNumber(getActivity());
+                doWork(phoneNumber);
+            }
+        }
+    }
+
+    public abstract void doWork(String phoneNumber);
+    public abstract Activity getActivity();
+
+    /**
+     * Get phone number of phone's   owner, this operation require READ_PHONE_STATE permission
+     * Because this is a dangerous permission, for API level 23 and above, we need to request this permission at runtime
+     *
+     * @param activity Activity that directly call this method or contain fragment call this method
+     * @return Phone number of the owner in String, return null if error happened
+     */
+    public String getPhoneNumber(Activity activity) {
+        // Request READ_PHONE_STATE permission (for API level 23 and above)
+
+        String phoneNumber = null;
+        // Check if user whether or not have this permission
+        int status = ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_PHONE_STATE);
+        if (status == PackageManager.PERMISSION_GRANTED) {
+            phoneNumber = getPhoneNumberOfUser(activity);
+            doWork(phoneNumber);
+        } else {
+            // Request permission
+            requestReadPhoneStatePermission(activity);
+        }
+
+        return phoneNumber;
+    }
+
+    private String getPhoneNumberOfUser(Activity activity) {
+        TelephonyManager telephonyManager = (TelephonyManager)activity.getSystemService(Context.TELEPHONY_SERVICE);
         return telephonyManager.getLine1Number();
+    }
+
+    private void requestReadPhoneStatePermission(Activity activity) {
+        String[] permissions = { Manifest.permission.READ_PHONE_STATE };
+        ActivityCompat.requestPermissions(activity, permissions, REQUEST_READ_PHONE_STATE);
     }
 }
