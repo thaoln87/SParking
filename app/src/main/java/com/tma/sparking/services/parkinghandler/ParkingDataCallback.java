96990b43276a60031ea4ef526639761652c03f27
Kim Huy <pkimhuy@tma.com.vn>
2017-06-05 14:58:38 +0700

    remove unnecessary callback and implement LoaderCallbacks in MainActivity

--- app/src/main/java/com/tma/sparking/services/parkinghandler/ParkingDataCallback.java
+++
@@ -1,9 +0,0 @@
-package com.tma.sparking.services.parkinghandler;
-
-import com.tma.sparking.models.ParkingField;
-
-import java.util.List;
-
-public interface ParkingDataCallback {
-    void onParkingFieldsLoaded(List<ParkingField> parkingFields);
-}
